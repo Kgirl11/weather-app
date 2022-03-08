66e49b5c5c54dd7e829d0e0e385f627cca52f173
Kgirl11 <50432157+Kgirl11@users.noreply.github.com>
2022-03-07 22:10:35 -0800

    Fahrenheit typo

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-let fahrenheitLink = documen.querySelector("#fahrenheit-link");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
