ac174cb7fe39fdf5872beef4a4a0c3ec9fab3748
Kgirl11 <50432157+Kgirl11@users.noreply.github.com>
2022-03-07 21:36:50 -0800

    Type correction in jS

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
+  console.log(response.data.daily);
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
@@ -53,7 +54,7 @@ function displayForecast(response) {
               alt=""
               width="42"
             />
-            <div class="weaether-forecast-temperatures">
+            <div class="weather-forecast-temperatures">
               <span class="weather-forecast-temperature-max">
                 ${Math.round(forecastDay.temp.max)}°{" "}
               </span>
@@ -72,9 +73,11 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "9bb2e2aab59857fad9fefd963f66a3b4";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -100,7 +103,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  //getForecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
