bd0f11cb2befe2cddfb444e1b821c6447eee121e
Kgirl11 <50432157+Kgirl11@users.noreply.github.com>
2022-03-07 22:16:03 -0800

    Fixed event listener

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ function displayFahrenheitTemperature(event) {
 
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
-  let displayFahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
